9de4a8fa462126b76709a9eb82d681fdf8a002a2
apvenkat <venkat.subramanian94@gmail.com>
2020-02-29 09:00:56 +0800

    temp

--- public/config.js
+++ public/config.js
@@ -91,14 +91,14 @@ $(function() {
           "/things/" + item.thingID + "/properties/temperature",
           function(json) {
             temp = json.Temperature;
-            temperature();
+            console.log(temp);
           }
         );
         output += '<div class="column">';
         output += '<div class="card">';
         output += "<h3>" + item.thingID + "</h3>";
         output += "<p>" + item.name + "</p>";
-        output += "<p>Temperature:" + temperature().temp + "</p>";
+        output += "<p>Temperature:" + temp + "</p>";
         output += "</div>";
         output += "</div>";
       }
