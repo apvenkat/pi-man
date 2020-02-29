e97036eb3c93849c6c4184f618bd894106305372
apvenkat <venkat.subramanian94@gmail.com>
2020-02-29 10:25:20 +0800

    temp

--- public/config.js
+++ public/config.js
@@ -92,15 +92,18 @@ $(function() {
           callback
         );
 
-        function callback(data) {
-          output += '<div class="column">';
-          output += '<div class="card">';
-          output += "<h3>" + item.thingID + "</h3>";
-          output += "<p>" + item.name + "</p>";
-          output += "<p>Temperature:" + data.Temperature + "</p>";
-          output += "</div>";
-          output += "</div>";
-        }
+        output += '<div class="column">';
+        output += '<div class="card">';
+        output += "<h3>" + item.thingID + "</h3>";
+        output += "<p>" + item.name + "</p>";
+        output +=
+          "<p>Temperature:" +
+          function callback(data) {
+            data.Temperature;
+          } +
+          "</p>";
+        output += "</div>";
+        output += "</div>";
       }
       // output += '     <div class="feedback-item item-list media-list">';
       // output += '       <div class="feedback-item media">';
